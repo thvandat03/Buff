efea5d0ba53069ac685cc5e96a554426aa547f03
thvandat03 <95754752+thvandat03@users.noreply.github.com>
2022-03-19 14:51:22 +0700

    Update tiktok.py

--- TopTop/tiktok.py
+++ TopTop/tiktok.py
@@ -1,4 +1,7 @@
-import
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 def initDriver(filePath):
   chromeOption = webdriver.chromeOptions()
   # config chrome with Path
